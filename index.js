33671fe94a189d9f9e6da5c53c7510139f2c4cc0
alnopa9 <argento.alyssa@gmail.com>
2019-02-13 23:37:02 -0500

    Reorganized file folders for XML markup plus updated site and server

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function init() {
 
             
             function rotate() {
-          var images = new Array ('pages/img/background/panda.png', 'pages/img/background/balloons.jpg', 'pages/img/background/bomb_hugger.jpg', 'pages/img/background/flower_thrower.png', 'pages/img/background/rat_camera.jpg', 'pages/img/background/rat_paint.jpg', 'pages/img/background/rat_parachute.jpg')
+          var images = new Array ('pages/img/background/slideshow/panda.png', 'pages/img/background/slideshow/balloons.png', 'pages/img/background/slideshow/bomb_hugger.png', 'pages/img/background/slideshow/flower_thrower.png', 'pages/img/background/slideshow/rat_camera.png', 'pages/img/background/slideshow/rat_paint.png', 'pages/img/background/slideshow/rat_parachute.png', 'pages/img/background/slideshow/anarchy_rat.png', 'pages/img/background/slideshow/angel.png', 'pages/img/background/slideshow/elephant_bomb.png', 'pages/img/background/slideshow/lenin.png', 'pages/img/background/slideshow/monkey_bomb.png', 'pages/img/background/slideshow/monkey_sign.png', 'pages/img/background/slideshow/rat_jackhammer.png', 'pages/img/background/slideshow/zebra.png')
                 var thisImage = 0;
       var thisImage = Math.floor(Math.random()*(images.length)) ;
 
           document.getElementById("rotator").src = images[thisImage];
 
-         setTimeout(rotate, 8 * 1000);
+         setTimeout(rotate, 5 * 1000);
           }
