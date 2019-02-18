6e82dd4b6ddd18d74e26500abaaee153db08e123
alnopa9 <argento.alyssa@gmail.com>
2019-02-18 14:26:13 -0500

    Updated page to be mobile-friendly

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function init() {
 
             
             function rotate() {
-          var images = new Array ('pages/img/background/slideshow/panda.png', 'pages/img/background/slideshow/balloons.png', 'pages/img/background/slideshow/bomb_hugger.png', 'pages/img/background/slideshow/flower_thrower.png', 'pages/img/background/slideshow/rat_camera.png', 'pages/img/background/slideshow/rat_paint.png', 'pages/img/background/slideshow/rat_parachute.png', 'pages/img/background/slideshow/anarchy_rat.png', 'pages/img/background/slideshow/angel.png', 'pages/img/background/slideshow/elephant_bomb.png', 'pages/img/background/slideshow/lenin.png', 'pages/img/background/slideshow/monkey_bomb.png', 'pages/img/background/slideshow/monkey_sign.png', 'pages/img/background/slideshow/rat_jackhammer.png', 'pages/img/background/slideshow/zebra.png')
+          var images = new Array ('pages/img/background/slideshow/panda.png', 'pages/img/background/slideshow/bomb_hugger.png', 'pages/img/background/slideshow/flower_thrower.png', 'pages/img/background/slideshow/rat_camera.png', 'pages/img/background/slideshow/rat_paint.png', 'pages/img/background/slideshow/rat_parachute.png', 'pages/img/background/slideshow/anarchy_rat.png', 'pages/img/background/slideshow/angel.png', 'pages/img/background/slideshow/elephant_bomb.png', 'pages/img/background/slideshow/lenin.png', 'pages/img/background/slideshow/monkey_bomb.png', 'pages/img/background/slideshow/monkey_sign.png', 'pages/img/background/slideshow/rat_jackhammer.png', 'pages/img/background/slideshow/zebra.png')
                 var thisImage = 0;
       var thisImage = Math.floor(Math.random()*(images.length)) ;
 
