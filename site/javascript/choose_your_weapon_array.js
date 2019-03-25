b09df92fe0d444f75f1fb0da34ec6b4242cfeb49
alnopa9 <argento.alyssa@gmail.com>
2019-03-25 18:29:33 -0400

    Pushing working paintings array!

--- site/javascript/choose_your_weapon_array.js
+++ site/javascript/choose_your_weapon_array.js
@@ -1,15 +1,12 @@
 window.onload = init
 function init() {
-    rotate();
+    rotateCYW();
 }
 
-            
-            function rotate() {
-          var images = new Array ('../img/paintings/choose_your_weapon_bright_purple.jpg', '../img/paintings/choose_your_weapon_dark_blue.jpg', '../img/paintings/choose_your_weapon_dark_orange.jpg', '../img/paintings/choose_your_weapon_gold.jpg', '../img/paintings/choose_your_weapon_green.jpg', '../img/paintings/choose_your_weapon_grey.jpg', '../img/paintings/choose_your_weapon_khaki.jpg', '../img/paintings/choose_your_weapon_lemon.jpg', '../img/paintings/choose_your_weapon_light_orange.jpg', '../img/paintings/choose_your_weapon_magenta.jpg', '../img/paintings/choose_your_weapon_olive.jpg', '../img/paintings/choose_your_weapon_que_jump_grey.jpg', '../img/paintings/choose_your_weapon_red.jpg', '../img/paintings/choose_your_weapon_silver.jpg', '../img/paintings/choose_your_weapon_sky.jpg', '../img/paintings/choose_your_weapon_slate.jpg', '../img/paintings/choose_your_weapon_soft_yellow.jpg', '../img/paintings/choose_your_weapon_turquoise.jpg', '../img/paintings/choose_your_weapon_white.jpg')
-                var thisImage = 0;
-      var thisImage = Math.floor(Math.random()*(images.length)) ;
-
-          document.getElementById("cyw_rotator").src = images[thisImage];
-
-         setTimeout(rotate, 3 * 1000);
-          }
+    function rotateCYW() {
+        var imagesCYW = new Array ('../img/paintings/choose_your_weapon_bright_purple.jpg', '../img/paintings/choose_your_weapon_dark_blue.jpg', '../img/paintings/choose_your_weapon_dark_orange.jpg', '../img/paintings/choose_your_weapon_gold.jpg', '../img/paintings/choose_your_weapon_green.jpg', '../img/paintings/choose_your_weapon_grey.jpg', '../img/paintings/choose_your_weapon_khaki.jpg', '../img/paintings/choose_your_weapon_lemon.jpg', '../img/paintings/choose_your_weapon_light_orange.jpg', '../img/paintings/choose_your_weapon_magenta.jpg', '../img/paintings/choose_your_weapon_olive.jpg', '../img/paintings/choose_your_weapon_que_jump_grey.jpg', '../img/paintings/choose_your_weapon_red.jpg', '../img/paintings/choose_your_weapon_silver.jpg', '../img/paintings/choose_your_weapon_sky.jpg', '../img/paintings/choose_your_weapon_slate.jpg', '../img/paintings/choose_your_weapon_soft_yellow.jpg', '../img/paintings/choose_your_weapon_turquoise.jpg', '../img/paintings/choose_your_weapon_white.jpg', '../img/paintings/choose_your_weapon_')
+            var thisImageCYW = 0;
+        var thisImageCYW = Math.floor(Math.random()*(imagesCYW.length)) ;
+        document.getElementById("cyw_rotator").src = imagesCYW[thisImageCYW];
+        setTimeout(rotateCYW, 3 * 1000);
+    }
